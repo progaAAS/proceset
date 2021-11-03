0730e89969160f897d355b60b1a754dca98b4fcf
AAS <gostand8@gmail.com>
2021-11-03 15:31:11 +0300

    Change

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,6 +1,6 @@
 import {combineReducers, createStore} from "redux";
 import userReducer from "./userReducer";
-import { reducer as formReducer } from 'redux-form';
+import { reducer as formReducer } from "redux-form";
 
 let reducers = combineReducers({
     userPage: userReducer,
